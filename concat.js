edbe8b170ed7749d7b03114f8d3fb03db5d593c7
theshantanujoshi <theshantanujoshi@users.noreply.github.com>
2026-07-17 02:22:42 +0530

    chore: remove extra test scripts and logs from git tracking

--- concat.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const schema = fs.readFileSync('supabase/migrations/20260714154200_initial_schema.sql', 'utf8');
-const seed = fs.readFileSync('seed.sql', 'utf8');
-
-const combined = schema + '\n\n' + seed;
-fs.writeFileSync('init-db.sql', combined, 'utf8');
-console.log('init-db.sql created successfully in utf8');
-console.log('init-db.sql created successfully in utf8');
